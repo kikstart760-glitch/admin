fab1589894f242db8493144cae697901994807ad
parnasree1 <parnasreepaul12@gmail.com>
2026-08-04 22:04:35 +0530

    admin page start

--- src/Components/LoginComponent/LoginComponent.jsx
+++ src/Components/LoginComponent/LoginComponent.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import '../LoginComponent/LoginComponent.css'
+import logo from '../../assets/logo.png'
 
 function LoginComponent() {
   return (
-    <div>LoginComponent</div>
+    <div className='behind-page'>
+      <div className="back">
+        <div className="overlay-content">
+          <div className="left-section">
+            <div className="logo-img">
+              <img src={logo} alt="Logo" />
+            </div>
+          </div>
+          <div className="right-section">
+            <div className="login-form">
+              <h2>Admin Login</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
